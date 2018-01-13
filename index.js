363f3f4f2dbb92dd4bb621f4bd5ffc2741119bfd
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2018-01-13 15:46:00 -0500

    Setting up paths

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const passport = require('passport');
 const flashConnect = require('connect-flash');
 const BodyParser = require('body-parser');
 
+//import your models here
+
 const app = express();
 app.use(BodyParser.json());
+
+//import your services here
+// import your routes here
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
